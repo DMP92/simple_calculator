987238ef4318d608cef50a7f98f703bd75023f28
DMP92 <devin@spirradigital.com>
2025-01-17 17:05:35 -0500

    Started to implement decimal system

--- src/printToScreen.js
+++ src/printToScreen.js
@@ -3,7 +3,6 @@ const total = document.querySelector('.calc_total');
 
 // Handle printing data to screen
 export default function printToScreen(option, mathematics) {
-    console.log(mathematics)
     if (option == 'generalComputations') {
         screen.textContent = `${mathematics.operand1} ${mathematics.operator} ${mathematics.operand2}`;
         total.textContent = `${mathematics.total}`
