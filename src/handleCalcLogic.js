87857b514c1f8b329c9b8bf7e41586f06ec92640
DMP92 <devin@spirradigital.com>
2025-01-24 10:30:38 -0500

    Added a decimal feature

--- src/handleCalcLogic.js
+++ src/handleCalcLogic.js
@@ -16,20 +16,13 @@ let mathematics = {
 
 function formatNumbers (num, total) {
     let tailFigure = ''
-    
     if(total.length != 0) {
         let headFigure = total[0].toString()
         tailFigure = tailFigure.concat(headFigure, num)
-        return total[0] = parseInt(tailFigure)
+        return total[0] = tailFigure;
     } else {
-        return total[0] = parseInt(num)
+        return total[0] = num;
     }
-}
-
-function formatString(str) {
-    // Might make sense to just add a conditional to the return statements in formatNumbers(num, total, isString)
-    // Add an isString argument or something similar so that I can keep everything exactly the same, limit the amount of
-    // code, but also I can easily integrate the decimal points without having to rewrite the app entirely.
 }
 
 function resetMath() {
@@ -45,69 +38,71 @@ export default function handleCalculatorLogic(button) {
     let type = button.getAttribute('button-type');
     let textContent = button.textContent;
     let mode = mathematics.operator
+
     if(type == 'operand') {
         if(mode == '=') { // if the last operation was the equal sign
             resetMath(); 
             mathematics.operator = ''
             mode = ''
         } 
+        
         if(mathematics.operator == '') { // If this is the first use of the calculator
-            // mathematics.operand1 = formatString(textContent)
-               mathematics.operand1 = formatNumbers(textContent, runningTotal)
+            if(textContent == '.') { // limit decimal points
+                if(mathematics.operand1.toString().indexOf('.') != -1) return
+            }
+            mathematics.operand1 = formatNumbers(textContent, runningTotal)
         } else {
-            // mathematics.operand2 = formatString(textContent)
-            mathematics.operand2 = formatNumbers(textContent, runningTotal);
-            handleMath(mathematics)
-        }
+                if(textContent == '.') { // limit decimal points
+                    if(mathematics.operand2.toString().indexOf('.') != -1) return
+                }
+                mathematics.operand2 = formatNumbers(textContent, runningTotal);
+                handleMath(mathematics)
+            }
     } else if (type == 'operator' & mathematics.operand1 != '') {
+        mode = textContent;
         if (mathematics.operand2 != '') {
             mathematics.operator = textContent;
-            runningTotal[0] = 0;
+            runningTotal[0] = '';
             mathematics.operand1 = mathematics.total
             mathematics.operand2 = ''
         } else {
             if(mode == '=') { resetMath(); }
             mathematics.operator = textContent;
-            runningTotal[0] = 0;
+            runningTotal[0] = '';
         }
     } else if (type == 'equalSign') {
         handleMath(mathematics)
         mode = '='
         mathematics.operator = '='
-    } else if(type == 'decimal') {
-        if(mathematics.operand2 == '') {
-            console.log(mathematics, 'operand2 is empty', runningTotal)
-        } else {
-            console.log(mathematics, 'operand2 is present')
-        }
     }
     else {
-        mathematics.operand1 = '0';
+        mathematics.operand1 = '';
         mathematics.operand2 = '';
         mathematics.operator = '';
-        mathematics.total = '0';
+        mathematics.total = '';
         runningTotal = []
     }
-    // console.log(mathematics, runningTotal)
-    // mathematics.operand2 = formatNumbers(textContent, runningTotal);
-    // handleMath(mathematics)
     printToScreen(mode, mathematics)
 }
 
 // Call Calculator module
 function handleMath() {
+    let a = Number(mathematics.operand1);
+    let b = Number(mathematics.operand2);
+
     switch(true) {
         case mathematics.operator == '/':
-            mathematics.total = calculator.divide(mathematics.operand1, mathematics.operand2);
+            mathematics.total = calculator.divide(a, b);
             break
         case mathematics.operator == '*':
-            mathematics.total = calculator.multiply(mathematics.operand1, mathematics.operand2);
+            mathematics.total = calculator.multiply(a, b);
             break
         case mathematics.operator == '-':
-            mathematics.total = calculator.subtract(mathematics.operand1, mathematics.operand2);
+            mathematics.total = calculator.subtract(a, b);
             break
         case mathematics.operator == '+':
-            mathematics.total = calculator.add(mathematics.operand1, mathematics.operand2);
+            mathematics.total = calculator.add(a, b);
             break
         }
 }
+
--- src/printToScreen.js
+++ src/printToScreen.js
@@ -3,13 +3,16 @@ const total = document.querySelector('.calc_total');
 
 // Handle printing data to screen
 export default function printToScreen(option, mathematics) {
-    if (option != '=') {
+    if (option != '=' && option != '') {
+        console.log(option, 'printToScreen - if', mathematics)
         screen.textContent = `${mathematics.operand1} ${mathematics.operator} ${mathematics.operand2}`;
         total.textContent = `${mathematics.total}`
     } else if (option == '') {
-        screen.textContent = `${mathematics.total}`
-        total.textContent = ``
+        console.log(option, 'printToScreen - else if', mathematics)
+        screen.textContent = `${mathematics.operand1} ${mathematics.operator}`
+        total.textContent = `${mathematics.operand1}`
     } else {
+        console.log(option, 'printToScreen - else', mathematics)
         screen.textContent = `${mathematics.total}`
         total.textContent = ``
     }
